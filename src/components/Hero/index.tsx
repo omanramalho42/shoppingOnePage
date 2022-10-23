951b125ca6e509a44528f0ed5a14a7c753fb85b5
oman ramalho <omanapple42@hotmail.com>
2022-10-23 12:44:57 -0300

    ajustando css componente hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -10,20 +10,20 @@ interface HeroProps {
 const Hero = ({ title, subtitle, btntext, img }: HeroProps) => {
   return (
     <div className='relative h-auto w-auto flex flex-col'>
-      <div className="bg-theme clip-path h-[85v] lg:h-[75vh] md:h-[65v] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10"></div>
-        <div className='relative opacity-100 z-20 grid justify-items-center shop-container'>
-        <div className='grid items-center justify-content-center mt-28 md:mt-24'>
+      <div className="bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10"></div>
+        <div className='relative opacity-100 z-20 grid items-center justify-items-center shop-container'>
+        <div className='grid items-center justify-items-center mt-28 md:mt-24'>
           <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>{ title }</h1>
           <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'> { subtitle } </h1>
-          <button type='button' className='button-theme bg-slate-200 shadow-slate-200 rounded-xl my-5'> { btntext } </button>
-          {/* <div></div> */}
-          {/* <div></div> */}
+          <button type='button' className='button-theme bg-slate-200  shadow-slate-200 rounded-xl my-5'> { btntext } </button>
+          {/* <div className="grid items-center gap-5 md:gap-3 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto"></div> */}
+          {/* <div className="grid items-center absolute top-[33vh] lg:top-[27vh] right-0 gap-3"></div> */}
         </div>
-        <div className=''>
+        <div className='flex items-center'>
           <img 
             className='w-auto h-[45vh] lg:h-[35vh] md:h-[31vh] sm:h-[21vh] xsm:h-[19vh] transitions-theme -rotate-[25deg] hover:rotate-0 cursor-pointer object-fill' 
             src={img} 
-            alt="image"
+            alt='hero-img/img'
           />
         </div>
       </div>
